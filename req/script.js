80a25d38996e09634b29954565359beec4f48483
RahulPOnline <rahulpal1798@gmail.com>
2025-12-25 00:20:49 +0530

    Basic routing req

--- req/script.js
+++ req/script.js
@@ -1,18 +1,46 @@
 const http = require('http')
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{                             //headers
-        "Content-Type":"application/json"
-    })
-    res.end(JSON.stringify({
-        id: 1,
-        title: "product 1"
-    }))
+const server = http.createServer((req, res) => {
+    // res.writeHead(200,{                             //headers
+    //     "Content-Type":"application/json"
+    // })
+    // res.end(JSON.stringify({
+    //     id: 1,
+    //     title: "product 1"
+    // }))
+
+    if (req.url == "/abcd") {
+        res.writeHead(200, {
+            "Content-Type": "application/json"
+        })
+
+        res.end(JSON.stringify({
+            id: 1,
+            name: "abcd",
+            url: 'www'
+        }))
+    }
+    else if (req.url == "/xyz") {
+        res.writeHead(200, {
+            "Content-Type": "application/json"
+        })
+        res.end(JSON.stringify({
+            id: 1,
+            name: "homepage"
+        }))
+
+    }
+    else {
+        res.writeHead(404, { "Content-Type": "text/plain" })
+        res.end("Not Found")
+    }
+
+
 })
 
 const PORT = 3000
 
-server.listen(PORT, ()=>{
+server.listen(PORT, () => {
     console.log(`server started at ${PORT}`)
-    
+
 })
