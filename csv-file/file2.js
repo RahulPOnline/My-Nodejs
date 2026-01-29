208c467da3818a9b671496cae2d607221d541980
RahulPOnline <rahulpal1798@gmail.com>
2026-01-29 20:11:10 +0530

    practise

--- csv-file/file2.js
+++ csv-file/file2.js
@@ -1,12 +1,30 @@
 const fs = require('fs')
-const {parse} = require('csv-parse')
+const { parse } = require('csv-parse')
 
-let productData=[]
+let productData = []
+
+function filterData(data) {
+    return data.category == "Fashion" && data.stock > 20 && data.price < 100
+}
 
 fs.createReadStream('ProductsData.csv')
 
-.pipe(parse({
-    comment:"#",
-    columns:true
-}))
+    .pipe(parse({
+        comment: "#",
+        columns: true
+    }))
 
+    .on("data", (data) => {
+        if(filterData(data))
+        {
+            productData.push(data)
+        }
+    })
+    .on("error", (error) => {
+        console.log("something wrong happend", error)
+    })
+    .on("end", () => {
+        console.log(productData.map((el)=>{
+            return el
+        }))
+    })
