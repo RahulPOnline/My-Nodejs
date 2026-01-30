4e99da9cc76c2f6cd0278ae03573dbbb31c43863
RahulPOnline <rahulpal1798@gmail.com>
2026-01-30 15:23:17 +0530

    routes practise

--- Expressjs/routesExample.js
+++ Expressjs/routesExample.js
@@ -1,13 +1,66 @@
 const express = require("express")
-const server = express()
+const app = express()
 
 
 
+app.get("/", (req, res) => {
+    res.send("Home Page")
+})
 
+app.get("/products", (req, res) => {
+    const products = [
+        {
+            id: 1,
+            category: "Fashion",
+            title: "Jeans"
+        },
+        {
+            id: 2,
+            category: "Office",
+            title: "Mouse"
+        },
+        {
+            id: 3,
+            category: "Electronics",
+            title: "Mobile"
+        }
+    ]
+    res.send(products)
+})
+
+app.get("/products/:id", (req, res) => {
+    const prodId = parseInt(req.params.id)
+
+    const products = [
+        {
+            id: 1,
+            category: "Fashion",
+            title: "Jeans"
+        },
+        {
+            id: 2,
+            category: "Office",
+            title: "Mouse"
+        },
+        {
+            id: 3,
+            category: "Electronics",
+            title: "Mobile"
+        }
+    ]
+    const singleProduct = products.find(products => products.id == prodId)
+
+    if (singleProduct) {
+        res.json(singleProduct)
+    }
+    else {
+        res.status(404).json({ error: "product is not found" })
+    }
+})
 
 
 
 const PORT = 3000
-server.listen(PORT,()=>{
-    console.log(`Server running at port ${PORT}`);    
+app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`);
 })
