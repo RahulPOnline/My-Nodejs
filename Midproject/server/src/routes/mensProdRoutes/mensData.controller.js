1e78da60c699a5aa525310ec0b8b7435cfe995fa
RahulPOnline <rahulpal1798@gmail.com>
2026-02-07 17:31:17 +0530

    modified

--- Midproject/server/src/routes/mensProdRoutes/mensData.controller.js
+++ Midproject/server/src/routes/mensProdRoutes/mensData.controller.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const mensData = require(path.join(__dirname,'..','..','models','mensData.model.js'))
 
-
 function getAllMens(req,res){
     return res.status(200).json(mensData)
 }
