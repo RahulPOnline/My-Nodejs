2a4b00f8e1f21c0d207b2de5b1fbbe3475e32e7f
RahulPOnline <rahulpal1798@gmail.com>
2026-02-08 17:47:12 +0530

    new route updated

--- MVC-with-routs/controllers/books.controller.js
+++ MVC-with-routs/controllers/books.controller.js
@@ -6,18 +6,62 @@ function getAllBooks(req, res) {
 
 function getAllBooksById(req, res) {
     let id = Number(req.params.id)
-    if (Number.isNaN(id) || id < 0 || id > booksArr.length) {
-        return res.status(404).json({ error: "data not found" })
+    const book = booksArr.find(el => el.id === id)
+    if (!book) {
+        return res.status(404).json({ error: "Data not found" })
     }
-    res.json(booksArr[id])
+    res.json(book)
 }
 
+function postBooksData(req, res) {
+    const entry = req.body
+    if (!(entry.title && entry.price)) {
+        return res.status(400).json({ error: "Invalid entry" })
+    }
+    entry.id = booksArr.length
+    booksArr.push(entry)
+    res.status(201).json(entry)
+}
 
+function deleteBooksDataById(req, res) {
+    const id = Number(req.params.id)
+    const index = booksArr.findIndex(el => el.id === id)
+    if (index === -1) {
+        return res.status(404).json({ error: "Resorce does'nt exist" })
+    }
+    booksArr.splice(index, 1)
+    res.json({ message: "deleted successfully" })
+}
 
+function patchBooksDataById(req, res) {
+    const id = Number(req.params.id)
+    const update = req.body
+    const index = booksArr.findIndex(el => el.id === id)
+    if (index === -1) {
+        return res.status(404).json({ error: "Data not found" })
+    }
+    booksArr[index] = { ...booksArr[index], ...update }
+    res.json({ message: "Data updated", data: booksArr[index] })
+}
+
+function putBooksDataById(req, res) {
+    const id = Number(req.params.id)
+    let update = req.body
+    let index = booksArr.findIndex(el => el.id === id)
+    if (index === -1) {
+        return res.status(404).json({ error: "Data not found" })
+    }
+    booksArr[index] = { ...update, id }
+    res.json({ message: "Data updated", data: booksArr[index] })
+}
 
 
 
 module.exports = {
     getAllBooks,
     getAllBooksById,
+    postBooksData,
+    deleteBooksDataById,
+    patchBooksDataById,
+    putBooksDataById
 }
--- MVC-with-routs/routes/books.routes.js
+++ MVC-with-routs/routes/books.routes.js
@@ -1,8 +1,12 @@
 const express = require("express")
-const { getAllBooks, getAllBooksById } = require("../controllers/books.controller")
+const { getAllBooks, getAllBooksById, postBooksData, deleteBooksDataById, patchBooksDataById, putBooksDataById } = require("../controllers/books.controller")
 const booksRoutes = express.Router()
 
 booksRoutes.get("/", getAllBooks)
 booksRoutes.get("/:id", getAllBooksById)
+booksRoutes.post("/", postBooksData)
+booksRoutes.delete("/:id", deleteBooksDataById)
+booksRoutes.patch("/:id", patchBooksDataById)
+booksRoutes.put("/:id", putBooksDataById)
 
 module.exports = booksRoutes
