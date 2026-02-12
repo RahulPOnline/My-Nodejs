2f703e1796ed064fca2384c5ca5d69656b42abfa
RahulPOnline <rahulpal1798@gmail.com>
2026-02-12 13:07:44 +0530

    updated

--- CRUD-with-mongoDB/usermodel.js
+++ CRUD-with-mongoDB/usermodel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 mongoose.connect(`mongodb://127.0.0.1:27017/mongopractice`)
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: String,
     username: String,
     email: String
